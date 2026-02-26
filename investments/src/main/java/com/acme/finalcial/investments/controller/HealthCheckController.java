22d217818d2c51b4248eed6491a32c18731a4cfd
KassioRF <kassio_rf@hotmail.com>
2026-02-26 10:28:18 -0300

    initialize repo

---
+++ investments/src/main/java/com/acme/finalcial/investments/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.acme.finalcial.investments.controller;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> healthCheck() {
+        return ResponseEntity.ok(Map.of("message", "Investments Service is up and running."));
+    }
+
+}
